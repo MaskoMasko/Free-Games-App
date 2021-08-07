44d8bccc62666475ef491d2f1c6ba48ae4547813
MaskoMasko <persic.massimo@gmail.com>
2021-08-07 21:35:12 +0200

    ahd a

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { getMovies } from "./api";
+
+interface Movie {
+  title: string;
+}
+
+const fetchData = async () => {
+  const movies = await getMovies();
+  movies.map((e: Movie) => console.log(e.title));
+};
 
 export default function App() {
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ config.tsx
@@ -0,0 +1,1 @@
+export const API_KEY = "b27cfa6975b477d35aff752392afd2bc";
