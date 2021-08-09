eebcad578d10ba89517de1166c7315793952f1aa
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-08-09 07:31:39 +0200

    nista

--- config.tsx
+++ config.tsx
@@ -1,1 +1,8 @@
 export const API_KEY = "b27cfa6975b477d35aff752392afd2bc";
+
+//mOJI KEYVE
+// API KEY(v3 auth) = b27cfa6975b477d35aff752392afd2bc
+// example api req:
+// https://api.themoviedb.org/3/movie/550?api_key=b27cfa6975b477d35aff752392afd2bc
+// api read access token(v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMjdjZmE2OTc1YjQ3N2QzNWFmZjc1MjM5MmFmZDJiYyIsInN1YiI6IjYxMGVkMjI5ZTFhZDc5MDA3NGQ5YTRhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aOsPihN9qiplkGPwFa5kkLm-Xz6cBsQCP2ATJE293i0
