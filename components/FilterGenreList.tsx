7ccb7566f274513d36393bb287f2358835af3ee2
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-08-10 14:59:29 +0200

    Update FilterGenreList.tsx

--- components/FilterGenreList.tsx
+++ components/FilterGenreList.tsx
@@ -1,27 +1,34 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 import { useQuery } from "react-query";
 import { store } from "../store/MoviesStore";
 
 export const FilterGenreList = observer(() => {
-  const { isLoading, isError, isIdle, data } = useQuery(
-    ["FilteredMovies"],
+  const { isLoading, isError, isIdle, data, isSuccess } = useQuery(
+    ["FilteredMovies", store.oneFatNothing],
     async () => {
       const sacekaj = await store.fetchMoviesByGenre(store.oneFatNothing);
       //it works sa samo rijci umisto value
       return sacekaj;
     }
   );
+  if (!isSuccess) {
+    return <Text>WAITING FOR YOU MOM TO COME</Text>;
+  }
   return (
-    <View>
-      {store.filteredMoviesByGenre.map((e, i) => {
+    <ScrollView>
+      {store.filteredMoviesByGenre.map((movie) => {
         return (
-          <View key={e.poster}>
-            <Text>{e.title}</Text>
+          <View key={movie.key}>
+            <Image
+              source={{ uri: movie.poster }}
+              style={{ width: 300, height: 450 }}
+            ></Image>
+            <Text>{movie.title}</Text>
           </View>
         );
       })}
-    </View>
+    </ScrollView>
   );
 });
