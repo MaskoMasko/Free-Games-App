fb5fb97502c0dd6f70437f4be70b5aacfaeedd90
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-08-10 13:20:22 +0200

    Update api.tsx

--- api/api.tsx
+++ api/api.tsx
@@ -1,45 +1,4 @@
 import { API_KEY } from "../config";
-const genres = [
-  { key: 12, genre: "Adventure" },
-  { key: 14, genre: "Fantasy" },
-  { key: 16, genre: "Animation" },
-  { key: 18, genre: "Drama" },
-  { key: 27, genre: "Horror" },
-  { key: 28, genre: "Action" },
-  { key: 35, genre: "Comedy" },
-  { key: 36, genre: "History" },
-  { key: 37, genre: "Western" },
-  { key: 53, genre: "Thriller" },
-  { key: 80, genre: "Crime" },
-  { key: 99, genre: "Documentary" },
-  { key: 878, genre: "Science Fiction" },
-  { key: 9648, genre: "Mystery" },
-  { key: 10402, genre: "Music" },
-  { key: 10749, genre: "Romance" },
-  { key: 10751, genre: "Family" },
-  { key: 10770, genre: "TV Movie" },
-  // "12": "Adventure",
-  // "14": "Fantasy",
-  // "16": "Animation",
-  // "18": "Drama",
-  // "27": "Horror",
-  // "28": "Action",
-  // "35": "Comedy",
-  // "36": "History",
-  // "37": "Western",
-  // "53": "Thriller",
-  // "80": "Crime",
-  // "99": "Documentary",
-  // "878": "Science Fiction",
-  // "9648": "Mystery",
-  // "10402": "Music",
-  // "10749": "Romance",
-  // "10751": "Family",
-  // "10752": "War",
-  // "10770": "TV Movie",
-];
-
-// type GenreId = keyof typeof genres;
 
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`;
 const getImagePath = (path: string) =>
